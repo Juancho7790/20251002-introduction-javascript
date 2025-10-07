8982cfca5882a485ad8bb4387d2d193dbd4a4b09
Juancho7790 <jd.orozco03@ciaf.edu.co>
2025-10-06 22:12:57 -0500

    Exercice 2

---
+++ exercises/ejercicio2.js
@@ -0,0 +1,16 @@
+const num1= 5.3;
+const num2= 23.9;
+
+console.log(`Número 1: ${num1}`)
+console.log(`Número 2: ${num2}`)
+
+const suma = num1 + num2;
+console.log(`${num1} + ${num2} = ${suma}`);
+
+const multi= num1 * num2;
+console.log(`${num1} * ${num2} = ${multi}`);
+
+const sumaRedondeada = Math.round(suma);
+console.log(`Suma redondeada: ${sumaRedondeada}`);
+const multiRedondeada = Math.round(multi);
+console.log(`Multiplicación redondeada: ${multiRedondeada}`);    
