1d4013e82b8295f0fbbfcac9d72576e5beae0ff6
Juancho7790 <jd.orozco03@ciaf.edu.co>
2025-10-06 22:51:24 -0500

    exercices 5

--- exercises/ejercicio3.js
+++ exercises/ejercicio3.js
@@ -3,22 +3,17 @@ console.log(`Rol asignado: "${rol}"\n`);
 
 switch (rol) {
     case "admin":
-        // Se ejecuta si rol === "admin"
         console.log("Acceso total");
         break; // break detiene la ejecución del switch
         
     case "user":
-        // Se ejecuta si rol === "user"
         console.log("Acceso limitado");
         break;
         
     case "guest":
-        // Se ejecuta si rol === "guest"
         console.log("Inicia sesión para continuar");
         break;
         
     default:
-        // Se ejecuta si ningún case coincide
-        // Es equivalente al 'else' en un if/else
         console.log("Rol no válido");
 }
---
+++ exercises/ejercicio5.js
@@ -0,0 +1,8 @@
+let sumaTotal = 0;
+console.log("Números del 1 al 20");
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+    sumaTotal += i;
+}
+console.log(`Suma total: ${sumaTotal}`);
